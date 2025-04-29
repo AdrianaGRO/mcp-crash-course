7ece1537029f3dc4ee7e24bb0e04c29f0bc77bb9
AdrianaGRO <adriana.gropan@gmail.com>
2025-04-29 16:06:24 +0300

    Add MCP integration and update dependencies
    - Integrated MCP client session and standard input/output server parameters in main.py.
    - Initialized ChatOpenAI and set up a basic async main function.
    - Added isort as a new dependency in pyproject.toml and updated uv.lock accordingly.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import asyncio
 from dotenv import load_dotenv
 import os
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
 
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/adricati/mcp/mcp-crash-course/servers/math_server.py"],
+)
 
 async def main():
     print("Hello from mcp-crash-course!")
